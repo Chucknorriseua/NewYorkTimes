01925587547e1feeec28ddbb2e238926287b7925
Yevhenii Poltavetc <lostyahoo565@gmail.com>
2025-02-01 20:02:38 +0200

    Update ArticleResults.swift

--- NYTNews/Model/ArticleResults.swift
+++ NYTNews/Model/ArticleResults.swift
@@ -45,10 +45,10 @@ struct ArticleResults: Identifiable, Codable, Hashable {
     
     mutating func sanitizeKeywords() {
         if let adxKeywords = adxKeywords {
-            self.adxKeywords = adxKeywords.replacingOccurrences(of: ";", with: "")
+            self.adxKeywords = adxKeywords.replacingOccurrences(of: ";", with: " ")
         }
-        self.title = title.replacingOccurrences(of: ";", with: "")
-        self.abstract = abstract.replacingOccurrences(of: ";", with: "")
+        self.title = title.replacingOccurrences(of: ";", with: " ")
+        self.abstract = abstract.replacingOccurrences(of: ";", with: " ")
     }
 }
 
