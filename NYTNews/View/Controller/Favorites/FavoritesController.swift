65619e145b8a791b3caa368a093a1cf53cad4c21
Yevhenii Poltavetc <lostyahoo565@gmail.com>
2025-02-02 18:41:12 +0200

    Update FavoritesController.swift

--- NYTNews/View/Controller/Favorites/FavoritesController.swift
+++ NYTNews/View/Controller/Favorites/FavoritesController.swift
@@ -19,11 +19,12 @@ struct FavoritesController: View {
                 LazyVStack {
                     ForEach(newsViewModel.favoritesArray, id: \.self) { article in
                         FavoritesDetailsView(newsVm: newsViewModel, favorites: article)
-                            .transition(.opacity)
+                            .transition(.scale)
                     }
                 }.padding(.top, 0)
                     .padding(.bottom, 60)
                     .padding(.horizontal, 8)
+                .transition(.scale)
             }.scrollIndicators(.hidden)
                 .background(.ultraThinMaterial)
                 .background(Color.mint.opacity(0.4))
